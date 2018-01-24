65aee073e041b57e64ed338074423ba23d795432
Patrick Juchli <patrickjuchli@gmail.com>
2018-01-24 17:19:23 +0100

    Speak of messages instead of groups

--- test/parseControlResponseSpec.js
+++ test/parseControlResponseSpec.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const groupControlResponse = require("../lib/ftp").utils.groupControlResponse;
+const parseControlResponse = require("../lib/ftp").utils.parseControlResponse;
 
 const CRLF = "\r\n";
 const LF = "\n";
@@ -9,42 +9,42 @@ describe("Parse multiline response", function() {
         {
             title: "Single line",
             res: `200 A`,
-            exp: { groups: [`200 A`], rest: "" }
+            exp: { messages: [`200 A`], rest: "" }
         },
         {
             title: "Single line with an extra CRLF",
             res: `200 A${CRLF}`,
-            exp: { groups: [`200 A`], rest: "" }
+            exp: { messages: [`200 A`], rest: "" }
         },
         {
             title: "Multiline: 1 response group",
             res: `150-A${CRLF}B${CRLF}150 C`,
-            exp: { groups: [`150-A${LF}B${LF}150 C`], rest: "" }
+            exp: { messages: [`150-A${LF}B${LF}150 C`], rest: "" }
         },
         {
             title: "Multiline: 2 response groups",
             res: `150-A${CRLF}B${CRLF}150 C${CRLF}200-D${CRLF}200 Done`,
-            exp: { groups: [`150-A${LF}B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
+            exp: { messages: [`150-A${LF}B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
         },
         {
             title: "Multiline: Invalid nested multiline",
             res: `150-A${CRLF}160-B${CRLF}150 C${CRLF}200-D${CRLF}200 Done`,
-            exp: { groups: [`150-A${LF}160-B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
+            exp: { messages: [`150-A${LF}160-B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
         },
         {
             title: "Multiline: Closing tag but indent",
             res: `150-A${CRLF} 150 B${CRLF}150 C${CRLF}200-D${CRLF}200 Done`,
-            exp: { groups: [`150-A${LF} 150 B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
+            exp: { messages: [`150-A${LF} 150 B${LF}150 C`, `200-D${LF}200 Done`], rest: "" }
         },
         {
             title: "Multline: No closing tag",
             res: `150-A${CRLF}160-B${CRLF}150 C${CRLF}200-D`,
-            exp: { groups: [`150-A${LF}160-B${LF}150 C`], rest: `200-D${LF}` }
+            exp: { messages: [`150-A${LF}160-B${LF}150 C`], rest: `200-D${LF}` }
         },               
     ];
     for (const test of tests) {
         it(test.title, function() {
-            const actual = groupControlResponse(test.res);
+            const actual = parseControlResponse(test.res);
             assert.deepEqual(actual, test.exp);
         });
     }
