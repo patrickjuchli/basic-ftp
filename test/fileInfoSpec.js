fb9858734b45673ad7e53c2cb18ae4786dd82533
Patrick Juchli <patrickjuchli@gmail.com>
2018-03-22 18:24:03 +0100

    Add tests for FileInfo getters

---
+++ test/fileInfoSpec.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+const FileInfo = require("../lib/FileInfo");
+
+describe.only("FileInfo", function() {
+    
+    it("can report type of file", function() {
+        const f = new FileInfo("");
+        f.type = FileInfo.Type.File;
+        assert(f.isFile);
+    });
+
+    it("can report type of directory", function() {
+        const f = new FileInfo("");
+        f.type = FileInfo.Type.Directory;
+        assert(f.isDirectory);
+    });
+
+    it("can report type of symbolic link", function() {
+        const f = new FileInfo("");
+        f.type = FileInfo.Type.SymbolicLink;
+        assert(f.isSymbolicLink); 
+    });
+});
