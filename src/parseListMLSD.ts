0b5b31f16b0f36d78e05ef3af6243d2a25978a84
Patrick Juchli <patrickjuchli@gmail.com>
2019-10-17 15:03:14 +0200

    Fix: MLSD type facts 'cdir', 'pdir' are not always present
    Directories '.' and '..' should be described by 'cdir' and 'pdir' MLSD
    types but it's possible that they're not.

--- src/parseListMLSD.ts
+++ src/parseListMLSD.ts
@@ -124,7 +124,7 @@ export function testLine(line: string): boolean {
  */
 export function parseLine(line: string): FileInfo | undefined {
     const [ packedFacts, name ] = splitStringOnce(line, " ")
-    if (name === "") {
+    if (name === "" || name === "." || name === "..") {
         return undefined
     }
     const info = new FileInfo(name)
