f5257b31f77630ddef5b0c2526c4491c577d6797
Patrick Juchli <patrickjuchli@gmail.com>
2020-01-03 00:17:53 +0100

    Refactor: Move function

--- src/transfer.ts
+++ src/transfer.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from "events"
-import { describeAddress, describeTLS } from "./netUtils"
+import { describeAddress, describeTLS, ipIsPrivateV4Address } from "./netUtils"
 import { Writable, Readable } from "stream"
 import { TLSSocket, connect as connectTLS } from "tls"
 import { FTPContext, FTPResponse, TaskResolver } from "./FtpContext"
@@ -76,23 +76,6 @@ export function parsePasvResponse(message: string): { host: string, port: number
         host: groups[1].replace(/,/g, "."),
         port: (parseInt(groups[2], 10) & 255) * 256 + (parseInt(groups[3], 10) & 255)
     }
-}
-
-/**
- * Returns true if an IP is a private address according to https://tools.ietf.org/html/rfc1918#section-3.
- * This will handle IPv4-mapped IPv6 addresses correctly but return false for all other IPv6 addresses.
- *
- * @param ip  The IP as a string, e.g. "192.168.0.1"
- */
-function ipIsPrivateV4Address(ip = ""): boolean {
-    // Handle IPv4-mapped IPv6 addresses like ::ffff:192.168.0.1
-    if (ip.startsWith("::ffff:")) {
-        ip = ip.substr(7) // Strip ::ffff: prefix
-    }
-    const octets = ip.split(".").map(o => parseInt(o, 10))
-    return octets[0] === 10                                             // 10.0.0.0 - 10.255.255.255
-        || (octets[0] === 172 && octets[1] >= 16 && octets[1] <= 31)    // 172.16.0.0 - 172.31.255.255
-        || (octets[0] === 192 && octets[1] === 168)                    // 192.168.0.0 - 192.168.255.255
 }
 
 export function connectForPassiveTransfer(host: string, port: number, ftp: FTPContext): Promise<void> {
