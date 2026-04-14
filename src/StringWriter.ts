5cb5367e86d8a2991224fb2b82e4933d27c07904
Patrick Juchli <patrickjuchli@gmail.com>
2026-04-14 22:36:19 +0200

    Add bounded StringWriter

--- src/StringWriter.ts
+++ src/StringWriter.ts
@@ -3,19 +3,28 @@ import { StringEncoding } from "./StringEncoding"
 
 export class StringWriter extends Writable {
 
-    protected buf = Buffer.alloc(0)
+    protected byteLength = 0
+    protected bufs: Buffer<ArrayBuffer>[] = []
+
+    constructor(protected maxByteLength: number = 1 * 1024 * 1024) {
+        super()
+    }
 
     _write(chunk: Buffer | string | any, _: string, callback: (error: Error | null) => void) {
-        if (chunk instanceof Buffer) {
-            this.buf = Buffer.concat([this.buf, chunk])
-            callback(null)
+        if (!(chunk instanceof Buffer)) {
+            callback(new Error("StringWriter: expects chunks of type 'Buffer'."))
+            return
+        }        
+        if (this.byteLength + chunk.byteLength > this.maxByteLength) {
+            callback(new Error(`StringWriter: Out of bounds. (maxByteLength=${this.maxByteLength})`))
+            return
         }
-        else {
-            callback(new Error("StringWriter expects chunks of type 'Buffer'."))
-        }
+        this.byteLength += chunk.byteLength
+        this.bufs.push(chunk)
+        callback(null)
     }
 
     getText(encoding: StringEncoding) {
-        return this.buf.toString(encoding)
+        return Buffer.concat(this.bufs).toString(encoding);
     }
 }
--- test/stringWriterSpec.js
+++ test/stringWriterSpec.js
@@ -3,7 +3,7 @@ const { StringWriter } = require("../dist/StringWriter")
 
 describe("StringWriter", function() {
     it("can run write string result", function() {
-        const w = new StringWriter()
+        const w = new StringWriter(10000)
         w.write(Buffer.from("hello"))
         w.write(Buffer.from("world"))
         w.end()
@@ -11,9 +11,19 @@ describe("StringWriter", function() {
     })
 
     it("can handle chunked multi-byte unicode codepoints", function() {
-        const w = new StringWriter()
+        const w = new StringWriter(10000)
         w.write(Buffer.from([0xE2, 0x82]))
         w.end(Buffer.from([0xAC]))
         assert.equal(w.getText("utf-8"), "€")
+    })
+
+    it("fails if out of bounds", function(done) {
+        const w = new StringWriter(10)
+        w.once("error", (err) => {
+            assert.match(err.message, /out of bounds/i)
+            done()
+        })
+        w.write(Buffer.from("hello"))
+        w.write(Buffer.from("worldworldworld"))
     })
 });
